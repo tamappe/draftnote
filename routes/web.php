28f4276584e7d2dd9e92ce7b7a7a83143ceacd7f
tamaoki <treasure6hunter9cole@gmail.com>
2018-10-19 01:02:43 +0900

    タスクの作成とページの表示まで

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,41 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Task;
+use Illuminate\Http\Request;
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Tasks
+
+// 一覧画面
+Route::get('/task', function () {
+    return view('task.index');
 });
+
+// 新タスク追加
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()){
+        return redirect('/task')->withInput()->withErrors($validator);
+    }
+
+    // タスク作成
+    $task = new Task;
+    $task->name = $request->name;
+    $task->text = "サンプル";
+    $task->save();
+
+    return redirect('/task');
+});
+
+// タスク削除
+Route::delete('/task/{task}', function (Task $task) {
+
+});
+
