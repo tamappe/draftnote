ff8470e313f62efe0c5ecb30616708eff0e51bf9
tamaoki <treasure6hunter9cole@gmail.com>
2018-10-30 21:26:04 +0900

    Revert "Analyticsの処理をもう一度トライする"
    This reverts commit be55dd55641f94f030dd0c5ca3d59a6d9eb27d2b.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,8 +15,7 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
-        // https://qiita.com/t_n/items/e2ab27684c6c323e0148
-//        $response->assertStatus(200);
-        $response->assertStatus(302);  // ステータスコードを変更
+
+        $response->assertStatus(200);
     }
 }
