65607ce2712df194abfd4dc724f2f4de583da0c6
tamaoki <treasure6hunter9cole@gmail.com>
2018-11-23 01:03:39 +0900

    タスクが全くない状態だとバグが発生するのでその修正

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -42,30 +42,35 @@
                 </div>
             </div>
 
-            <button class="btn btn-default" data-toggle="modal" data-target="#modal-new">
-                <i class="fa fa-btn fa-plus"></i>新規プロジェクト
-            </button>
+            @if(count($tasks) > 0)
+                <button class="btn btn-default" data-toggle="modal" data-target="#modal-new">
+                    <i class="fa fa-btn fa-plus"></i>新規プロジェクト
+                </button>
 
-            @include('layouts.modal-new')
+                @include('layouts.modal-new')
+                @php
+                    // これをキーにしてプロジェクトのタイトルやidなどをセットしていく
+                    $first_task = $tasks[0];
+                @endphp
+                @if($first_task->project != null && $first_task->project->user->current_project_id != null)
+                    @include('layouts.modal-edit', ['project' => $first_task->project])
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">
+                            {{ $first_task->project->title}} <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            @foreach($first_task->project->user->projects as $project)
+                                @if($project->id != $first_task->project->user->current_project_id)
+                                    <li><a href="{{ url('task/change/' .$project->id) }}">{{ $project->title }}</a></li>
+                                @endif
+                            @endforeach
+                            <li><a data-toggle="modal" data-target="#modal-edit">名前の編集</a></li>
+                        </ul>
 
-            @if($tasks[0]->project->user->current_project_id != null)
-                @include('layouts.modal-edit', ['project' => $tasks[0]->project])
-                <div class="btn-group">
-                    <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                        {{ $tasks[0]->project->title}} <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        @foreach($tasks[0]->project->user->projects as $project)
-                            @if($project->id != $tasks[0]->project->user->current_project_id)
-                                <li><a href="{{ url('task/change/' .$project->id) }}">{{ $project->title }}</a></li>
-                            @endif
-                        @endforeach
-                        <li><a data-toggle="modal" data-target="#modal-edit">名前の編集</a></li>
-                    </ul>
-
-                    </button>
-                </div>
+                        </button>
+                    </div>
+                @endif
             @endif
 
         <!-- 現在のタスク -->
